39e220a8ac0b20f9959e6bffcf98e3b09145a70a
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-04-06 09:00:28 -0600

    Agregar archivos de proyecto.

---
+++ L01P02-2021-HL-650/Models/db_notasContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace L01P02_2021_HL_650.Models
+{
+    public class db_notasContext : DbContext
+    {
+        public db_notasContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
